a04ac8b4367856fedf594ad0113c4d0907d3e579
nikhil-kumar787 <nikhilkumar7167@gmail.com>
2021-07-31 10:38:08 +0530

    Initial commit

---
+++ src/containers/HomePage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+function HomePage() {
+    return (
+        <div>
+            <Layout />
+        </div>
+    )
+}
+
+export default HomePage
